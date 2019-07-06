ab79e8c06f3c1e5067d3dd028d2b8fd9d2b82090
Yuriy Danko <danko98uran@gmail.com>
2019-07-06 08:26:39 +0300

    Initial commit

---
+++ App/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Program
+    {
+        static  void Main(string[] args)
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+            var token = cancellationTokenSource.Token;
+            ShowCurrentTimeAsync(token);
+            Console.ReadKey();
+            cancellationTokenSource.Cancel();
+            ConfirmExit(token);
+            Console.ReadKey();
+            cancellationTokenSource.Cancel();
+            Console.ReadKey();
+        }
+
+        private static void ShowCurrentTime()
+        {
+            while (true)
+            { 
+                Console.WriteLine($"Current date and time is {DateTime.Now}");
+                Thread.Sleep(8000);
+            }
+        }
+
+        private static async Task ShowCurrentTimeAsync(CancellationToken cancellationToken)
+        {
+            await Task.Run(() => ShowCurrentTime(), cancellationToken);
+            if (cancellationToken.IsCancellationRequested)
+            {
+                Console.WriteLine("Cancellation request");
+            }
+        }
+
+        private static async Task ConfirmExit(CancellationToken cancellationToken)
+        {
+            await Task.Run(() => Console.WriteLine("thanks a lot for using our service," +
+                                                   " press any key to confirm exit"), cancellationToken);
+            if (cancellationToken.IsCancellationRequested)
+            {
+                Console.WriteLine("Cancellation request");
+                
+            }
+        }
+    }
+}
