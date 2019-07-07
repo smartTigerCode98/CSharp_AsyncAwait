a626c94476afc44799fd7c1abe9759bb866b3c2d
Yuriy Danko <danko98uran@gmail.com>
2019-07-07 21:02:11 +0300

    Changed logic in method

--- App/Program.cs
+++ App/Program.cs
@@ -10,25 +10,21 @@ namespace App
         {
             var cts = new CancellationTokenSource();
             var token = cts.Token;
-            PrintCurrentTimeAsync(token);
+            Task.Run(() => PrintCurrentTimeAsync(token), token);
             Console.ReadKey();
             ConfirmExitAsync(token);
             Console.ReadKey();
             cts.Cancel();
         }
-        
-        private static async Task PrintCurrentTimeAsync(CancellationToken cancellationToken)
+
+        private static void PrintCurrentTimeAsync(CancellationToken cancellationToken)
         {
-            var task = Task.Run(() =>
-                {
-                    while (!cancellationToken.IsCancellationRequested)
-                    {
-                        Console.WriteLine($"Current date and time is {DateTime.Now}");
-                        Thread.Sleep(8000);
-                    }
-                },
-                cancellationToken);
-            await task;
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Console.WriteLine($"Current date and time is {DateTime.Now}");
+                Thread.Sleep(8000);
+            }
         }
 
         private static async Task ConfirmExitAsync(CancellationToken cancellationToken)
